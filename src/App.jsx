9e2da3f5fb453b195eea2bec5bebee8df9b983c2
richel265 <richel265@github.com>
2025-04-07 19:48:02 -0400

    hello react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <nav>
+        <div class="logo">
+          <a href="/">
+            <img src="/images/logo.png" alt="ByePassHub logo" class="logoimg" />
+          </a>
+        </div>
+        <ul class="navlist">
+          <li>
+            <a href="/" class="navlink">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/Cheats.html" class="navlink">
+              Cheats
+            </a>
+          </li>
+          <li>
+            <a href="/Games.md" class="navlink">
+              Games
+            </a>
+          </li>
+          <li>
+            <a href="/mainUnblockers.md" class="navlink">
+              Websites
+            </a>
+          </li>
+          <li>
+            <a href="/Exploits/README.md" class="navlink">
+              Exploits
+            </a>
+          </li>
+          <li>
+            <a href="/Exploits/Bookmarklets/Bookmarklets.md" class="navlink">
+              {" "}
+              Bookmarklets{" "}
+            </a>
+          </li>
+          <li>
+            <a href="/Movies&Anime.md" class="navlink">
+              Movies&Anime
+            </a>
+          </li>
+          <li>
+            <a href="/README.md" class="navlink">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="document">
+        <h2>ByePassHub | Main Menu</h2>
+        <h2>If you like this collection, make sure to star it! - 230 stars!</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is a collection of links of proxies, games, exploits, bypasses,
+          AI, movies, and apps.
         </p>
+        <br />
+        <p></p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
